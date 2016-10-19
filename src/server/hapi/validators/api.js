26aa49eb954904d88354ac97af587fbf5b8f98de
nick.peleh <nick.peleh@gmail.com>
2016-10-19 19:04:46 +0300

    Update validation module

--- src/server/hapi/validators/api.js
+++ src/server/hapi/validators/api.js
@@ -1,33 +1,14 @@
-var Joi = require('joi');
+const Joi = require('joi');
 
 module.exports = Joi.object({
-    _id: Joi.number().optional(),
+    age: Joi.number().integer(),
     name: Joi.string().min(2).max(30).required(),
     gender: Joi.string().min(4).max(4),
-    title: Joi.string().max(30),
-    phone_number: Joi.string().max(30),
+    company: Joi.string().max(30),
     email: Joi.string().email(),
-    street_address: Joi.string().max(100),
-    city: Joi.string().max(100),
-    state: Joi.string().alphanum().max(30),
-    zip_code: Joi.string().max(30),
+    phone: Joi.string().max(30),
+    address: Joi.string().max(100),
+    about: Joi.string().max(100),
     created: Joi.date(),
     updated: Joi.date()
 });
-
-//module.exports = {
-//    contact: Joi.object({
-//        _id: Joi.number().optional(),
-//        name: Joi.string().min(2).max(30).required(),
-//        gender: Joi.string(4),
-//        title: Joi.string().max(30),
-//        phone_number: Joi.string().max(30),
-//        email: Joi.string().email(),
-//        street_address: Joi.string().max(100),
-//        city: Joi.string().max(100),
-//        state: Joi.string().alphanum().max(30),
-//        zip_code: Joi.string().max(30),
-//        created: Joi.date(),
-//        updated: Joi.date()
-//    })
-//};
