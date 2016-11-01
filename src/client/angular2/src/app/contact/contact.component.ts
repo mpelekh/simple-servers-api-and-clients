490faa5ae2614f1a6f741dc6cf61f4fe879a6b6a
nick.peleh <nick.peleh@gmail.com>
2016-11-01 22:28:13 +0200

    Update contact component

--- src/client/angular2/src/app/contact/contact.component.ts
+++ src/client/angular2/src/app/contact/contact.component.ts
@@ -1,15 +1,40 @@
-import { Component, OnInit, EventEmitter, Input, Output } from '@angular/core';
+import { Component, ChangeDetectionStrategy, OnInit, EventEmitter, Input, Output } from '@angular/core';
+import { Contact } from './contact.model';
 
 @Component({
     selector: 'contact',
-    templateUrl: 'contact.component.html'
+    templateUrl: 'contact.component.html',
+    styleUrls: ['contact.styles.css'],
+    changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ContactComponent implements OnInit {
-    @Input() contacts: Array<any>;
-    @Output() add = new EventEmitter();
-    
-    constructor() { }
+    @Input() contact: Contact;
+    @Output() remove = new EventEmitter();
+    @Output() edit = new EventEmitter();
+    isEditMode: boolean = false;
+    newName: string;
 
-    ngOnInit() { }
+    ngOnInit() {
+        this.contact = Object.assign({}, this.contact);
+        this.newName = this.contact.name;
+    }
+
+    removeContact(contact: Contact, event: any) {
+        this.remove.emit({ contact });
+    }
+
+    editContact(event: any) {
+        this.isEditMode = true;
+    }
+
+    applyEdit(newName: string, event: any) {
+        this.contact = Object.assign({}, this.contact, {name: this.newName});
+        this.isEditMode = false;
+        this.edit.emit({ contact: this.contact });
+    }
+
+    cancelEdit(event: any){
+        this.isEditMode = false;
+    }
 }
 
---
+++ src/client/angular2/src/app/contact/contact.model.ts
@@ -0,0 +1,15 @@
+export interface Contact {
+  readonly _id: string;
+  about: string;
+  address: string;
+  age: number;
+  company: string;
+  created: string;
+  email: string;
+  gender: string;
+  guid: string;
+  isActive: boolean;
+  name: string;
+  phone: string;
+  picture: string;
+}
