c47ccf2c3b09146d091934b87360bffeef043a2d
nick.peleh <nick.peleh@gmail.com>
2016-11-11 20:40:41 +0200

    Add new route for edit contact

--- src/client/angular2/src/app/app.routes.ts
+++ src/client/angular2/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { ContactsListViewComponent } from './contacts-list-view/contacts-list-view.component';
 import { ContactDetailViewComponent } from './contact-detail-view/contact-detail-view.component';
+import { ContactEditViewComponent } from './contact-edit-view/contact-edit-view.component';
 import { Routes } from '@angular/router';
 
 export const AppRoutes: Routes = [
   { path: '', component: ContactsListViewComponent },
-  { path: 'contact/:id', component: ContactDetailViewComponent }
+  { path: 'contact/:id', component: ContactDetailViewComponent },
+  { path: 'contact/:id/edit', component: ContactEditViewComponent }
 ];
